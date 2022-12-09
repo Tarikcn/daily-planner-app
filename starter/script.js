644a97105d21275ea0ed4a57cc53911b3a1b93ee
unknown <khamliche@gmail.com>
2022-12-09 16:42:25 +0000

    add getlocalstorage code

--- starter/script.js
+++ starter/script.js
@@ -8,4 +8,18 @@ $(function () {
     var dateTime = dayjs();
     displayedCurrentTime.text(dateTime.format("dddd, MMMM DD, h:mm:ss , YYYY"));
   }, 1000);
+
+  /* Functions*/
+  //----Get local stocal storage data and display in browser
+  function renderData() {
+    timeBlockListEl.each(function () {
+      var keyName = $(this).children().eq(0).text();
+      var remoteValue = localStorage.getItem(keyName);
+      if (remoteValue) {
+        $(this).children(".description").val(remoteValue);
+      }
+    });
+  }
+
+  /* Event listener*/
 });
